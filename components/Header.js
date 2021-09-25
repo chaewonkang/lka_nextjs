c78d10efd3636d859d4ca6dcb4a67c0a946a209c
Chaewon Kang <saturday.the17th@gmail.com>
2021-09-25 14:17:53 +0900

    HotFix

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,25 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 function Header() {
+  const router = useRouter();
+
   return (
     <div className='header_container'>
       <div className='header_inner'>
         <div>
           <Link href='/about'>
-            <h1>About Artist Workshop, Becoming-Local</h1>
+            {router.asPath.includes('about') ? (
+              <Link href='/'>
+                <h1 style={{ fontStyle: 'italic' }}>
+                  About Artist Workshop, Becoming-Local
+                </h1>
+              </Link>
+            ) : (
+              <Link href='/about'>
+                <h1>About Artist Workshop, Becoming-Local</h1>
+              </Link>
+            )}
           </Link>
         </div>
         <div>
