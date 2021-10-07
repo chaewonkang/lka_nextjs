1c2e8035937b02cb8b9f28349bd0c83e709ed075
Chaewon Kang <saturday.the17th@gmail.com>
2021-10-07 18:36:56 +0900

    HotFix

--- pages/index.js
+++ pages/index.js
@@ -1,140 +1,283 @@
 import { ThemeProvider } from 'styled-components';
-import { useEffect, useState } from 'react';
-
+import { useEffect, useState, useRef } from 'react';
 import theme from '../styles/theme';
 import PageLayout from '../components/PageLayout';
-import Link from 'next/link';
+import jQuery from 'jquery';
 
-const imgArray = [
-  '../static/images/example.png',
-  '../static/images/example2.png',
+const dataSet = [
+  {
+    index: 0,
+    flag: 'taiki',
+    artist: 'Taiki Sakpisit',
+    keyword: [
+      'Alchemical Transmutation',
+      'Disembodiment',
+      'Divination',
+      'Eschatology',
+      'Mysticism',
+      'Phantasmagoria',
+      'Sensory Stimulus',
+      'Spectrality',
+    ],
+    thumb: '../static/images/example1.png',
+  },
+  {
+    index: 1,
+    flag: 'wonjung',
+    artist: 'Wonjung Shin',
+    keyword: [
+      'Abject',
+      'Dialog',
+      'Gesture',
+      'Network',
+      'Non-Verbal',
+      'Noting',
+      'Stress',
+      'Thing-In-Itself',
+    ],
+    thumb: '../static/images/example2.png',
+  },
+  {
+    index: 2,
+    flag: 'sabina',
+    artist: 'Sabina Hyoju AHN',
+    keyword: [
+      'Ai',
+      'Air',
+      'Akademies',
+      'Covid19',
+      'Daily Life',
+      'Digital',
+      'Forest',
+      'Germany',
+      'Learning',
+      'Local',
+      'Lockdown',
+      'Machine',
+      'Residency',
+      'Schloss',
+      'Solitude',
+      'Stuttgart',
+      'Walk',
+    ],
+    thumb: '../static/images/example3.png',
+  },
+  {
+    index: 3,
+    flag: 'aamp',
+    artist: 'AAMP',
+    keyword: [
+      'Badvibes',
+      'Denvicky',
+      'Findings',
+      'Intro2barter',
+      'Narratives',
+      'Patkay',
+      'Shady',
+      'Waiting',
+    ],
+    thumb: '../static/images/example4.png',
+  },
+  {
+    index: 4,
+    flag: 'minjung',
+    artist: 'Minjung Kim',
+    keyword: [
+      'Afforest',
+      'Becalm',
+      'Blithesome',
+      'Buffoonish',
+      'Causerie',
+      'Chivalrous',
+      'Congratulatory',
+      'Dapper',
+      'Whisper',
+    ],
+    thumb: '../static/images/example5.png',
+  },
+  {
+    index: 5,
+    flag: 'john',
+    artist: 'John Torres',
+    keyword: [
+      'Apple',
+      'Extremum',
+      'Featherbrained',
+      'Felicity',
+      'Ism',
+      'Indefinite',
+      'Nuance',
+      'Majestic',
+      'Sappy',
+      'Spiffy',
+      'Timbre',
+      'Zippy',
+    ],
+    thumb: '../static/images/example2.png',
+  },
 ];
 
 const Index = () => {
-  const [artist1, setArtist1] = useState(false);
-  const [artist2, setArtist2] = useState(false);
+  const [keyword, setKeyword] = useState('');
   const [loading, setLoading] = useState(true);
+  const [thumbUrl, setThumbUrl] = useState('');
+  const [flag, setFlag] = useState('');
+  const [prevFlag, setPrevFlag] = useState('');
+
+  let keywordArr = [].concat.apply(
+    [],
+    dataSet.map((item) => item.keyword)
+  );
 
   useEffect(() => {
+    window.$ = window.jQuery = jQuery;
+
     setTimeout(() => {
       setLoading(true);
     }, 500);
-  }, [artist1, artist2, loading]);
+
+    dataSet.map((item) => {
+      if (item.keyword.includes(keyword)) {
+        setFlag(item.flag);
+        setPrevFlag(item.flag);
+        setThumbUrl(item.thumb);
+      } else return null;
+    });
+
+    const flagArray = document.getElementsByClassName(flag);
+    const italicTarget = [...flagArray];
+
+    // if (flag !== prevFlag)
+    //   italicTarget.map((item) => (item.style.fontStyle = 'italic'));
+    // else italicTarget.map((item) => (item.style.fontStyle = 'normal'));
+  }, [keyword, thumbUrl, flag, loading]);
 
   return (
     <ThemeProvider theme={theme}>
       {loading && (
         <>
           <PageLayout>
-            {artist1 && (
+            {thumbUrl && (
               <div className='thumbnail_container'>
-                <img id='thumbnail' src={imgArray[0]}></img>
-              </div>
-            )}
-            {artist2 && (
-              <div className='thumbnail_container'>
-                <img id='thumbnail' src={imgArray[1]}></img>
+                <img id='thumbnail' src={thumbUrl}></img>
               </div>
             )}
             <div className='keyword_container'>
               <div>
-                <span
-                  onMouseEnter={() => {
-                    setArtist1(true);
-                    console.log(`onMouseEnter: ${artist1}`);
-                  }}
-                  onMouseLeave={() => {
-                    setArtist1(false);
-                    console.log(`onMouseAway: ${artist1}`);
-                  }}
-                  style={artist1 ? { fontStyle: 'italic' } : null}
-                >
-                  Abject
-                </span>
-                <span
-                  onMouseEnter={() => {
-                    setArtist2(true);
-                    console.log(`onMouseEnter: ${artist1}`);
-                  }}
-                  onMouseLeave={() => {
-                    setArtist2(false);
-                    console.log(`onMouseAway: ${artist1}`);
-                  }}
-                  style={artist2 ? { fontStyle: 'italic' } : null}
-                >
-                  Ai
-                </span>
-                <span>Air</span>
-                <span>Akademies</span>
-                <span>
-                  Alchemical
-                  <br />
-                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Transmutation
-                </span>
-                <span>Artist</span>
-                <span>Badvibes</span>
-                <span>Border</span>
-                <span>Caring</span>
-                <span>Connected</span>
-                <span>Covid19</span>
-                <span>Daily Life</span>
-                <span>Denvicky</span>
-                <span>Dialog</span>
-                <span>Digital</span>
+                {keywordArr &&
+                  keywordArr
+                    .sort()
+                    .slice(0, 15)
+                    .map((item, index) => {
+                      return (
+                        <span
+                          className={dataSet
+                            .map((el) => {
+                              if (el.keyword.includes(item)) return el.flag;
+                            })
+                            .filter((el) => {
+                              if (el != ',') return el;
+                            })}
+                          key={item}
+                          onMouseEnter={() => {
+                            setKeyword(item);
+                          }}
+                          onMouseLeave={() => {
+                            setKeyword('');
+                            setFlag('');
+                          }}
+                        >
+                          {item}
+                        </span>
+                      );
+                    })}
               </div>
               <div>
-                <span>Digital</span>
-                <span>Disembodiment</span>
-                <span>Divination</span>
-                <span>Entanglement</span>
-                <span>Ergliffenheit</span>
-                <span>Eschatology</span>
-                <span>Findings</span>
-                <span>Forest</span>
-                <span>Foresta</span>
-                <span>Germany</span>
-                <span>Gesture</span>
-                <span>Interaction</span>
-                <span>Interlock</span>
-                <span>Intro2barter</span>
-                <span>Learning</span>
-                <span>Local</span>
+                {keywordArr &&
+                  keywordArr
+                    .sort()
+                    .slice(15, 31)
+                    .map((item, index) => {
+                      return (
+                        <span
+                          className={dataSet
+                            .map((el) => {
+                              if (el.keyword.includes(item)) return el.flag;
+                            })
+                            .filter((el) => {
+                              if (el != ',') return el;
+                            })}
+                          key={item}
+                          onMouseEnter={() => {
+                            setKeyword(item);
+                          }}
+                          onMouseLeave={() => {
+                            setKeyword('');
+                            setFlag('');
+                          }}
+                        >
+                          {item}
+                        </span>
+                      );
+                    })}
               </div>
               <div>
-                <span>Lockdown</span>
-                <span>Machine</span>
-                <span>Minority</span>
-                <span>Language</span>
-                <span>Mountain</span>
-                <span>Mysticism</span>
-                <span>Narratives</span>
-                <span>Native</span>
-                <span>Network</span>
-                <span>Non-Verbal</span>
-                <span>Noting</span>
-                <span>Onta</span>
-                <span>Patkay</span>
-                <span>Phantasmagoria</span>
-                <span>Residency</span>
-                <span>Roam</span>
+                {keywordArr &&
+                  keywordArr
+                    .sort()
+                    .slice(31, 47)
+                    .map((item, index) => {
+                      return (
+                        <span
+                          className={dataSet
+                            .map((el) => {
+                              if (el.keyword.includes(item)) return el.flag;
+                            })
+                            .filter((el) => {
+                              if (el != ',') return el;
+                            })}
+                          key={item}
+                          onMouseEnter={() => {
+                            setKeyword(item);
+                          }}
+                          onMouseLeave={() => {
+                            setKeyword('');
+                            setFlag('');
+                          }}
+                        >
+                          {item}
+                        </span>
+                      );
+                    })}
               </div>
               <div>
-                <span>Schloss</span>
-                <span>Sensory Stimulus</span>
-                <span>Settle Down</span>
-                <span>Shady</span>
-                <span>Solitude</span>
-                <span>Sound</span>
-                <span>Sound</span>
-                <span>Spectrality</span>
-                <span>Stress</span>
-                <span>Stuttgart</span>
-                <span>Thing-In-Itself</span>
-                <span>Tide</span>
-                <span>Touch The Ground</span>
-                <span>Waiting</span>
-                <span>Walk</span>
-                <span>Xeo</span>
+                {keywordArr &&
+                  keywordArr
+                    .sort()
+                    .slice(47, 65)
+                    .map((item, index) => {
+                      return (
+                        <span
+                          key={item}
+                          className={dataSet
+                            .map((el) => {
+                              if (el.keyword.includes(item)) return el.flag;
+                            })
+                            .filter((el) => {
+                              if (el != ',') return el;
+                            })}
+                          onMouseEnter={() => {
+                            setKeyword(item);
+                          }}
+                          onMouseLeave={() => {
+                            setKeyword('');
+                            setFlag('');
+                          }}
+                        >
+                          {item}
+                        </span>
+                      );
+                    })}
               </div>
             </div>
             <div className='footer_container'>
